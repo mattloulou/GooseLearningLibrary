5e8d02ca33764d6da75aaa46e3c18282d92d59ac
Evan He <e7he@uwaterloo.ca>
2021-10-01 19:03:37 -0400

    Added NeuralNet class

--- Connection.cpp
+++ Connection.cpp
@@ -1,6 +1,1 @@
 #include "Connection.h"
-
-struct Connection {
-    double weight_;
-    double delta_weight_;
-};
---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include "NeuralNet.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout<<"Test\n";
+    return 0;
+}
