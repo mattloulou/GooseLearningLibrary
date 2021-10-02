6ec8af5e2d5ed076b43ec5c6853d29473cb76f2a
mattloulou <mattloulou123123@gmail.com>
2021-10-01 22:48:13 -0400

    added xor example to main

--- main.cpp
+++ main.cpp
@@ -3,7 +3,41 @@
 
 using namespace std;
 
+void xorTest()
+{
+    //creating the topology for the network
+    std::vector<size_t> topology;
+    topology = { 2,2,1 };
+
+    NeuralNet my_net(topology);
+
+    //defining variables
+    unsigned numOfTests = 10000;
+    std::vector<double> input_vals = { 0, 0 };
+    std::vector<double> target_vals = { 0 };
+    std::vector<double> result_vals = { 0 };
+
+    //doing the training
+    for (int i = 1; i <= numOfTests; ++i) {
+        std::cin >> input_vals[0];
+        std::cin >> input_vals[1];
+        std::cin >> target_vals[0];
+        std::cout << "Inputs #" << i << ": " << input_vals[0] << " " << input_vals[1] << std::endl;
+        std::cout << "Target Value: " << target_vals[0] << std::endl;
+
+        my_net.FeedForward(input_vals);
+        my_net.BackPropagate(target_vals);
+        result_vals = my_net.GetResults();
+
+        result_vals[0] = (result_vals[0] < 0.5) ? 0 : 1;
+
+        std::cout << "Guessed Results: " << result_vals[0] << std::endl;
+        std::cout << "Error: " << my_net.GetError() << std::endl << std::endl;
+    }
+}
+
+
 int main() 
 {
-
+    xorTest();
 }
